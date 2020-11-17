8791429c546ea0be62e62d6ac64862da8877638c
Ken Goettler <goettlek@gmail.com>
2020-11-17 08:11:30 -0800

    Begin string handling functions

---
+++ test/test_str.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "str.h"
+
+int main (void)
+{
+    double vals[] = {
+        1,
+        100,
+        12e3,
+        15.9E9,
+        154.5E12
+    };
+    for (int i = 0; i < 5; i++)
+    {
+        char *sval = double_to_ss(vals[i]);
+        printf("%s\n", sval);
+        free(sval);
+    }
+    return 0;
+}
